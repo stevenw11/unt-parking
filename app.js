1e0b99950028b6b0dd1cba6230bd452f1e0f6182
Alejandro Olvera <alejandroolvera70@gmail.com>
2016-10-05 14:17:56 -0500

    updated folders and script styling

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 // create the module and name it forumApp
 // also include ngRoute for all our routing needs
-    var forumApp = angular.module('forumApp',['ngRoute']);
+var parkingApp = angular.module('parkingApp',['ngRoute']);
 
-    // configure our routes
-    forumApp.config(function($routeProvider) {
-        $routeProvider
-            // route for the home page
-            .when('/', {
-                templateUrl : 'views/home/home.html',
-                controller  : 'mainController',
-	    	}) 
+// configure our routes
+parkingApp.config(function($routeProvider) {
+	$routeProvider
+	// route for the home page
+    .when('/', {
+		templateUrl : 'views/Home/home.html',
+		controller  : 'mainController',
+	}) 
 
-	    // we must add new routes with each page so here's a basic structure
-	    // the html in a page would look like. . . 
-	    // . . . <a href="#/<address>"> <page></a>  . . . .
-	    // it is important to include the '#' symbol as this tells the js where to inject
-	    // .when('/<address>', {
-            //     templateUrl : 'views/<path-to-html-file-you-want-to-use>',
-	    //     controller : '<page>Controller'
-	    // });
+// we must add new routes with each page so here's a basic structure
+// the html in a page would look like. . . 
+// . . . <a href="#/<address>"> <page></a>  . . . .
+// it is important to include the '#' symbol as this tells the js where to inject
+// .when('/<address>', {
+//     templateUrl : 'views/<path-to-html-file-you-want-to-use>',
+//     controller : '<page>Controller'
+// });
+
 });
---
+++ scripts/navbar.js
@@ -0,0 +1,12 @@
+// Functions to operate navigation bar
+function openNav() {
+	document.getElementById("side-bar").style.width = "200px";
+	document.getElementById("main-bar").style.marginLeft = "200px";
+	document.getElementById("sidebar-toggle").innerHTML = '<a href="javascript:void(0)" onclick="closeNav()"><i class="glyphicon glyphicon-remove"></i></a>';
+}
+
+function closeNav() {
+	document.getElementById("side-bar").style.width = "0px";
+	document.getElementById("main-bar").style.marginLeft = "0px";
+	document.getElementById("sidebar-toggle").innerHTML = '<a href="javascript:void(0)" onclick="openNav()"><i class="glyphicon glyphicon-menu-hamburger"></i></a>';
+}
